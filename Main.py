7661cbbe9bc2f521d0168080c2bbe88c97bd29d9
titanite07 <124230249+titanite07@users.noreply.github.com>
2023-12-25 09:10:34 +0530

    Main Code

--- Main.py
+++ Main.py
@@ -45,20 +45,11 @@ def solves(puzzle):
     return False
 
 if __name__ == '__main__':
-    board = [
-        [ 0, 0, 0,   2, 6, 0,   7, 0, 1],
-        [ 6,  8, 0,   0, 7, 0,   0, 9, 0,],
-        [ 1,  9, 0,   0, 0, 4,   5, 0, 0],
-
-        [ 8,  2, 0,   1, 0, 0,   0, 4, 0],
-        [ 0,  0, 4,   6, 0, 2,    9, 0, 0],
-        [ 0, 5, 0,   0, 0, 3,    0, 2, 8,],
-
-        [0, 0, 9,    3, 0, 0,   0, 7, 4],
-        [0, 4, 0,   0, 5, 0,   0, 3, 6],
-        [ 7, 0, 3,   0,  1,  8,   0, 0, 0]
-    ]
-    
+    board = []
+    print("Enter 9x9 Sudoku (separated by commas):")
+for _ in range(9):
+    row = [int(x) for x in input().strip().replace(',', ' ').split()]
+    matrix.append(row)
 
     print(solves(board))
     pprint(board)
